b8c922b8ddf2e7fc9dfea5415f30132106ece977
Deivid Willyan <deiviid_@hotmail.com>
2016-02-12 13:20:44 -0200

    Create GmailSend-0.2.py

---
+++ GmailSend/GmailSend-0.2.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# Deivid Willyan
+# Envio de Emails Com Python!
+# V0.2 - 12/02/2016
+# Melhoria no Codigo.
+# Adicionado threading
+
+# INFO :  https://www.google.com/settings/security/lesssecureapps  > Permitir Dispositivos Menos Seguros "Ativar"
+
+import smtplib
+from threading import Thread
+
+def envio(dst):
+    usr = 'deividwillyan.email@gmail.com'
+    pwd = 'python123'
+
+    server = smtplib.SMTP('smtp.gmail.com:587')
+    server.starttls()
+    server.login(usr, pwd)
+
+    msg = 'Mensagem De Teste Python'
+    assunto = 'Assunto Python'
+    format = 'Subject: %s\n\n%s' % (assunto, msg)
+
+    server.sendmail(usr, dst, format)
+    server.quit()
+
+def main():
+    dst = input('Destinatario: ')
+    for env in range(10):
+        t = Thread(target=envio, args=(dst,))
+        t.start()
+    print('Envio Concluido!')
+
+if __name__ == '__main__':
+    main()
